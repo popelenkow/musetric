b8c1e9570d53ee7d4ce0d3b6ada6a6de4cc96f98
Vladlen Popelenkov <popelenkow@gmail.com>
2026-03-15 16:42:46 +0700

    Implement applyPatchConfig

---
+++ packages/audio/src/common/patchConfig.es.ts
@@ -0,0 +1,45 @@
+type IsValueEqual<Value> = (first: Value, second: Value) => boolean;
+
+export type ApplyPatchConfigOptions<Config> = {
+  base?: Config;
+  draft?: Partial<Config>;
+  patch: Partial<Config>;
+  isEqual?: {
+    [Key in keyof Config]?: IsValueEqual<Config[Key]>;
+  };
+};
+
+const isDefaultEqual = (first: unknown, second: unknown) => first === second;
+
+export const applyPatchConfig = <Config>(
+  options: ApplyPatchConfigOptions<Config>,
+) => {
+  const { base, patch } = options;
+
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  const keys = Object.keys(patch) as (keyof Config)[];
+
+  const draft: Partial<Config> = { ...options.draft };
+
+  keys.forEach((key) => {
+    const isEqual = options.isEqual?.[key] ?? isDefaultEqual;
+    const baseValue = base?.[key];
+    const draftValue = draft[key];
+    const patchValue = patch[key];
+
+    if (patchValue === undefined) {
+      return;
+    }
+    if (baseValue !== undefined && isEqual(baseValue, patchValue)) {
+      delete draft[key];
+      return;
+    }
+    if (draftValue !== undefined && isEqual(draftValue, patchValue)) {
+      return;
+    }
+
+    draft[key] = patchValue;
+  });
+
+  return draft;
+};
---
+++ packages/audio/src/spectrogram/common/patchConfig.ts
@@ -0,0 +1,22 @@
+import {
+  applyPatchConfig,
+  type ApplyPatchConfigOptions,
+} from '../../common/patchConfig.es.js';
+import type { SpectrogramConfig } from '../config.es.js';
+
+export const applySpectrogramPatchConfig = (
+  options: ApplyPatchConfigOptions<SpectrogramConfig>,
+) =>
+  applyPatchConfig({
+    base: options.base,
+    draft: options.draft,
+    patch: options.patch,
+    isEqual: options.isEqual ?? {
+      viewSize: (first, second) =>
+        first.width === second.width && first.height === second.height,
+      colors: (first, second) =>
+        first.background === second.background &&
+        first.played === second.played &&
+        first.unplayed === second.unplayed,
+    },
+  });
