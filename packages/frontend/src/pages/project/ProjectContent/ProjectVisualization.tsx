64b7fc4689394eb19c565052b674de80be1e6568
Vladlen Popelenkov <popelenkow@gmail.com>
2026-05-05 09:01:34 +0700

    Unify waveform and spectrogram seek handling

--- packages/frontend/src/pages/project/ProjectContent/ProjectVisualization.tsx
+++ packages/frontend/src/pages/project/ProjectContent/ProjectVisualization.tsx
@@ -1,20 +1,40 @@
 import { Box } from '@mui/material';
 import { type FC } from 'react';
+import { engine } from '../../../engine/engine.js';
 import { SpectrogramCanvas } from '../spectrogram/SpectrogramCanvas.js';
 import { useProjectStore } from '../store.js';
+import { getVisualizationFrameIndex } from '../visualization/visualizationSeek.js';
+import {
+  hiddenTrackListScrollbarSx,
+  useTrackListScroll,
+} from '../waveform/useTrackListScroll.js';
 import { WaveformList } from '../waveform/WaveformList.js';
 
 export const ProjectVisualization: FC = () => {
   const visualizationMode = useProjectStore((state) => state.visualizationMode);
+  const detailsMode = useProjectStore((state) => state.detailsMode);
+  const containerRef = useTrackListScroll(visualizationMode === 'waveform');
 
   return (
     <Box gridArea='visualization'>
-      {visualizationMode === 'waveform' && <WaveformList />}
-      {visualizationMode === 'spectrogram' && (
-        <Box flexGrow={1} flexBasis={0} height='100%' overflow='hidden'>
-          <SpectrogramCanvas />
-        </Box>
-      )}
+      <Box
+        ref={containerRef}
+        height='100%'
+        overflow='auto'
+        onClick={(event) => {
+          const newFrameIndex = getVisualizationFrameIndex(event);
+          if (newFrameIndex === undefined) return;
+          engine.player.seek(newFrameIndex);
+        }}
+        sx={
+          visualizationMode === 'waveform' && detailsMode === 'mixer'
+            ? hiddenTrackListScrollbarSx
+            : undefined
+        }
+      >
+        {visualizationMode === 'waveform' && <WaveformList />}
+        {visualizationMode === 'spectrogram' && <SpectrogramCanvas />}
+      </Box>
     </Box>
   );
 };
